c377a84969871d21ae93eec0571625b02a02a06f
Jorge Moron <moronjorge94@gmail.com>
2021-12-15 17:39:11 -0400

    otro cambio en el titulo del formulario ver productos

--- VerProductos.php
+++ VerProductos.php
@@ -27,7 +27,7 @@ $resultado = mysqli_query($conectar, $sql);
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Listas</title>
+    <title>Listas de productos</title>
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
     <link href="css/styles.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
